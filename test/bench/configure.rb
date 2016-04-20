277721edbc8d370e0c7b1bbe92298fa1246c85e4
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2016-04-20 14:00:29 -0500

    Argument errors are raised when required parameters are not passed in

--- test/bench/configure.rb
+++ test/bench/configure.rb
@@ -30,12 +30,27 @@ context "Defining the configure method on a class" do
       assert receiver.some_dependency_name.optional_keyword_arg == optional_keyword_arg
     end
 
+    context "Required positional argument is not passed in" do
+      test "Raises error" do
+        assert proc { control_class.configure receiver, required_keyword_arg: required_keyword_arg, optional_keyword_arg: optional_keyword_arg } do
+          raises_error? ArgumentError
+        end
+      end
+    end
+
     context "Optional positional argument is not passed in" do
       control_class.configure receiver, required_positional_arg, required_keyword_arg: required_keyword_arg, optional_keyword_arg: optional_keyword_arg
 
-
       test "Optional argument is nil" do
         assert receiver.some_dependency_name.optional_positional_arg.nil?
+      end
+    end
+
+    context "Required keyword argument is not passed in" do
+      test "Raises error" do
+        assert proc { control_class.configure receiver, required_positional_arg, optional_positional_arg } do
+          raises_error? ArgumentError
+        end
       end
     end
 
