ea58795f34f05422187e4722eb1b15496eb2d634
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2016-04-20 13:51:02 -0500

    Combine keyword arguments and positional arguments into one control class

--- lib/configure/controls/configured/with_arguments.rb
+++ lib/configure/controls/configured/with_arguments.rb
@@ -1,16 +1,20 @@
 module Configure
   module Controls
     module Configured
-      module KeywordArguments
+      module WithArguments
         class Example
           extend Configure::Macro
 
           configure :some_dependency_name
 
+          attr_reader :required_positional_arg
+          attr_reader :optional_positional_arg
           attr_reader :required_keyword_arg
           attr_reader :optional_keyword_arg
 
-          def initialize(required_keyword_arg:, optional_keyword_arg: nil)
+          def initialize(required_positional_arg, optional_positional_arg=nil, required_keyword_arg:, optional_keyword_arg: nil)
+            @required_positional_arg = required_positional_arg
+            @optional_positional_arg = optional_positional_arg
             @required_keyword_arg = required_keyword_arg
             @optional_keyword_arg = optional_keyword_arg
           end
--- test/bench/configure.rb
+++ test/bench/configure.rb
@@ -13,43 +13,34 @@ context "Defining the configure method on a class" do
     end
   end
 
-  context "Class has positional arguments" do
-    control_class = Configure::Controls::Configured::PositionalArguments.example
-    positional_arg = 'a'
-    optional_arg = 'b'
-
-    test "Class is configured as a dependency and positional arguments are set" do
-      control_class.configure receiver, positional_arg, optional_arg
-
-      assert receiver.some_dependency_name.is_a?(control_class)
-      assert receiver.some_dependency_name.positional_arg == positional_arg
-      assert receiver.some_dependency_name.optional_arg == optional_arg
-    end
-
-    context "Optional positional argument is not passed in" do
-      control_class.configure receiver, positional_arg
-
-      test "Optional argument is nil" do
-        assert receiver.some_dependency_name.optional_arg.nil?
-      end
-    end
-  end
-
-  context "Class has keyword arguments" do
-    control_class = Configure::Controls::Configured::KeywordArguments.example
+  context "Class has arguments" do
+    control_class = Configure::Controls::Configured::WithArguments.example
+    required_positional_arg = 'a'
+    optional_positional_arg = 'b'
     required_keyword_arg = 'c'
     optional_keyword_arg = 'd'
 
-    test "Class is configured as a dependency and keyword arguments are set" do
-      control_class.configure receiver, required_keyword_arg: required_keyword_arg, optional_keyword_arg: optional_keyword_arg
+    test "Class is configured as a dependency and positional arguments are set" do
+      control_class.configure receiver, required_positional_arg, optional_positional_arg, required_keyword_arg: required_keyword_arg, optional_keyword_arg: optional_keyword_arg
 
       assert receiver.some_dependency_name.is_a?(control_class)
+      assert receiver.some_dependency_name.required_positional_arg == required_positional_arg
+      assert receiver.some_dependency_name.optional_positional_arg == optional_positional_arg
       assert receiver.some_dependency_name.required_keyword_arg == required_keyword_arg
       assert receiver.some_dependency_name.optional_keyword_arg == optional_keyword_arg
     end
 
+    context "Optional positional argument is not passed in" do
+      control_class.configure receiver, required_positional_arg, required_keyword_arg: required_keyword_arg, optional_keyword_arg: optional_keyword_arg
+
+
+      test "Optional argument is nil" do
+        assert receiver.some_dependency_name.optional_positional_arg.nil?
+      end
+    end
+
     context "Optional keyword argument is not passed in" do
-      control_class.configure receiver, required_keyword_arg: required_keyword_arg
+      control_class.configure receiver, required_positional_arg, optional_positional_arg, required_keyword_arg: required_keyword_arg
 
       test "Optional keyword argument is nil" do
         assert receiver.some_dependency_name.optional_keyword_arg.nil?
